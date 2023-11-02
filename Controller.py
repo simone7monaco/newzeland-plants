21b2156a2216df855b27b507fd9e29c70c053897
simone7monaco <simone7monaco@gmail.com>
2023-11-02 17:33:34 +0100

    initial commit

---
+++ Controller.py
@@ -0,0 +1,141 @@
+import streamlit as st
+from pathlib import Path
+import seaborn as sns
+import json
+
+from streamlit_tags import st_tags
+from annotated_text import annotated_text
+from utils import *
+
+processed_path = Path(__file__).parent / "processed_features.csv"
+species = pd.read_excel(Path(__file__).parent / "plant_info.xlsx").set_index('SpeciesName')
+species['Features'] = species['Features'].fillna('')
+
+with open(Path(__file__).parent / "acceptances.json") as f:
+    acceptances = json.load(f)
+
+def get_color(score, fixed_color=True):
+    from colorsys import rgb_to_hsv, hsv_to_rgb
+    if fixed_color: return colors[score.argmax()]
+    h = colors[score.argmax()].lstrip("#")
+
+    c = rgb_to_hsv(*tuple(int(h[i:i+2], 16) for i in (0, 2, 4)))
+    c = [c[0], c[1], 255-((255 - c[2]) * (score.max()))]
+    c = [int(ch) for ch in hsv_to_rgb(*c)]
+
+    return '#%02x%02x%02x' % tuple(c)
+
+def refresh_processed_features():
+    processed_features = species.Features.apply(string_preprocessing).str.split(r'(?<!\sc)[.;]\s|;').reset_index().apply(lambda x: extract_features(x.SpeciesName, x.Features), axis=1)
+    processed_features = processed_features.applymap(lambda x: '; '.join(x) if not isinstance(x, float) else x)
+    processed_features.index = species.index
+
+    processed_features.to_csv(processed_path)
+    get_unlabelled_measures(processed_features)
+
+    with open(Path(__file__).parent / "acceptances.json", "w") as f:
+        json.dump({sp: '' for sp in anomalies}, f) # TODO: is this available from utils?
+    return processed_features
+
+def update_acceptance(toggles):
+    for k, v in toggles.items():
+        if v:
+            acceptances[k] = 'correct'
+        else:
+            acceptances[k] = ''
+            # if acceptances.get(k, '') == 'correct':
+                # acceptances.pop(k)
+    with open(Path(__file__).parent / "acceptances.json", "w") as f:
+        json.dump(acceptances, f)
+
+
+def main():
+    st.title("Extracted features control")
+    st.write("This page is used to control the extracted features from the plant_info.xlsx file")
+
+    if st.button("Refresh", type="primary"):
+        refresh_processed_features()
+    
+    # data = st.file_uploader("Carica il file", type=["doc", "docx"])
+    # now the file to take is plant_info.xlsx
+
+    if processed_path.exists():
+        processed_features = pd.read_csv(processed_path, index_col=0)
+    else:
+        processed_features = refresh_processed_features()
+
+    with open(Path(__file__).parent / "acceptances.json") as f:
+        acceptances = json.load(f)
+        
+
+    get_color = lambda i: sns.color_palette("husl", len(processed_features.columns)).as_hex()[i]
+
+    features_colors = {feat: get_color(i) for i, feat in enumerate(processed_features.columns)} | {'null': '#808080'}
+
+    page_length = 25
+    processed_features_anom = processed_features[processed_features.index.isin(acceptances.keys())]
+    page_number = st.selectbox("Page number", range(0, len(processed_features_anom), page_length), index=0)
+
+    processed_view = processed_features_anom.iloc[page_number:page_number+page_length]
+    st.text(f"Showing species from {page_number} to {page_number+page_length}")
+    toggles = {}
+    bar = st.progress(0)
+    for i, (species_name, feature) in enumerate(processed_view.iterrows()):
+        bar.progress(i/len(processed_view))
+        features_text = species.loc[species_name, 'Features'].replace('\xa0', ' ').replace('×', 'x').replace('–', '-')
+        if features_text == '': continue
+        # print in the center of the page a bold title with the shown species (ids from ** to **)
+        
+
+        # TODO: create 3 tabs, one with all the remaining texts, one with the accepted, one with the errors
+        if acceptances.get(species_name, '') == 'correct':
+            st.header(f":green[{species_name}]")
+            continue
+
+        st.header(f"{species_name}")
+        toggles[species_name] = st.toggle('Mark as correct', key=f"{species_name}_toggle", value=True if acceptances.get(species_name, '') == 'correct' else False) 
+
+        
+        
+
+        measures_in_text = re.finditer(r'\d[\d\.\s\(\)x-]*[cmd]?m\s?(?![\d-\(\)])', features_text)
+        
+        detected_features = []
+        for match in measures_in_text:
+            which_feature = feature[feature.apply(lambda x: isinstance(x, str) and string_preprocessing(match.group()).strip() in x.split('; '))]
+            if which_feature.empty:
+                detected_features.append(('null', match.group(), match.start(), match.end()))
+            else:
+                for feat in which_feature.index:
+                    detected_features.append((feat, match.group(), match.start(), match.end()))
+        detected_features = sorted(detected_features, key=lambda x: x[2])
+
+        coloured_text = []
+        last_found = 0
+        for feat, raw, start, end in detected_features:
+            coloured_text.append(features_text[last_found:start])
+            coloured_text.append((features_text[start:end], feat, features_colors[feat]))
+            last_found = end
+
+        if last_found != len(features_text):
+            coloured_text.append(features_text[last_found:])
+        
+        annotated_text(*coloured_text)
+        # write in bold
+        st.markdown(f"**Extracted features**")
+        annotated_text(*[(meas, feat, features_colors[feat]) for meas, feat in zip(feature, feature.index) if isinstance(meas, str)])
+    
+    with st.sidebar:    
+        submit = st.button("Submit", type="primary")
+        if submit:
+            update_acceptance(toggles)
+            st.success("Submitted!")
+        
+        st.header("Marked as wrong")
+        for k, v in toggles.items():
+            if v:
+                st.write(k)
+        
+        
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,131 @@
+import regex as re
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+
+species = pd.read_excel(Path(__file__).parent / "plant_info.xlsx").set_index('SpeciesName')
+species['Features'] = species['Features'].fillna('')
+
+extra_features_df = pd.read_excel("Words before and after traits.xlsx", sheet_name="Words", skiprows=1)
+extra_features_df = extra_features_df[:extra_features_df[extra_features_df.Stature.eq('Following words')].index[0]].applymap(lambda s:s.lower() if type(s) == str else s)
+
+extra_features = extra_features_df.to_dict('list')
+extra_features = {k.title().replace(' ', ''): [x for x in v if str(x) != 'nan'] for k, v in extra_features.items()}
+all_words = extra_features_df.to_numpy().flatten()
+# duplicated words
+duplicate_words = []
+for word in set(all_words):
+	if (all_words == word).sum() > 1:
+		duplicate_words.append(word)
+print(duplicate_words)
+words_to_remove = []
+# remove all words starting with petal, flower, fruit, seed with a letter after different from s. Select the entire word up to the first space or punctuation (,;.) excluded
+reg_exp = r'(petal|flower|fruit|seed)[a-rt-z]+[^\s;,.\)]'
+for i, row in species.Features.items():
+	# print(row)
+	for word in re.finditer(reg_exp, row):
+		words_to_remove.append(word.group())
+
+words_to_remove = list(set(words_to_remove))
+
+
+unit = '[m|c|d]?m'
+# 150–400x100–300mm
+number = r"(\d+\.?\d*)"
+# full_regex = rf"(({number}-)?{number}{unit}?x)?{number}(-{number})?{unit}" ## Supposed to be correct
+"""anomaies:
+	0.05-0.35-1mx1.5-3-6mm
+"""
+full_regex = rf"({number}(-{number})?(-{number})?{unit}?x)?({number}(-{number})?(-{number})?{unit}?x)?{number}(-{number})?(-{number})?(-{number})?{unit}"
+
+
+def string_preprocessing(s):
+	s = s.replace('\xa0', ' ').replace('×', 'x').replace('–', '-') # remove non-breaking space and replace multiplication sign with x
+	s = re.sub(fr'-?\(-?{number}-?\)-?', '', s) # remove all parentesis surrounding a number (e.g. (-1.5))
+	s = s.replace('--', '-').replace('-.', '-').replace('..', '.')
+	s = s.replace('(', '').replace(')', '')
+	s = re.sub(r'\s(c|ca|o)\.', ' ', s) # remove all ' c.'
+	s = re.sub(r'\s+(?=[cmd]?m)', '', s) # remove all spaces before measures (mm, cm, dm, m, these strings only if padded by a space)
+	s = re.sub(r'\s*-\s*', '-', s) # remove spaces around hyphens
+	s = re.sub(r'(?<=\d)\s*\.(?=\d)', '.', s) # remove spaces before dot if followed and preceded by a number
+	s = re.sub(r'(?<=\s)\.(?=\d)', '0.', s) # add a 0 before a dot if it is preceded by a space and a "not number" and followed by a number (e.g. foo .5 --> foo 0.5)
+	s = re.sub(r'(?<=[\dm])\s*x\s*(?=\d+)', 'x', s) # remove spaces around x in formulas
+	# now all measures are supposed to have no spaces between number and unit and spaces around them
+	s = re.sub(rf'(?<=\d)({unit})(?!\s*x)', r'\1 ', s) # fix situation in which a measure is not followed by a space, in the case, add that space
+	s = re.sub(r'(?<![\d\sx\.-])(\d)', r' \1', s) # fix the situation in which a measure (the whole number and measure) is not preceded by a space. In the case, add a space before the measure
+	s = re.sub(r'(?<=\d\.\d+)(\.\d?)', '', s) # fix the error in which there is a doubled dot in a number (e.g. 1.5.2), in the case, remove the second dot and the eventual numbers after it
+	s = re.sub(r'(?<=[a-ln-z])-(?=\d)', ' ', s) # remove all '-' preceded by a letter (different from m) and followed by a number (e.g. to-250mm --> to 250mm)
+	s = re.sub(r'(?<![a-z])(l|I)(?=[\s\.-]|\d)', '1', s) # replace all 'l' or "I" characters which should be '1' (e.g. l.5 --> 1.5). This should be followed by a space, a dot, a hyphen, or a number and not preceded by a letter
+	s = re.sub('|'.join(words_to_remove), '', s, flags=re.IGNORECASE) # remove words to remove
+	return s
+
+
+tmp = []
+anomalies = set() # species with anomalies
+
+def extract_features(i, feats:list, wordmeasure_distance=100): # TODO: automatic wordmeasure_distance
+	features = {}
+	for feat in feats:
+		if len(feat) < 1:
+			continue
+		found = False
+		for key, values in extra_features.items():
+			feat = feat.replace(',', ' ')
+			# remove any of .,; after one of the measures (cm, mm, dm, m), keeping the measure
+			feat = re.sub(rf'{unit}[\.,;]', lambda x: x.group()[:-1], feat)
+			if any([w == wf for w in set(values) for wf in feat.lower().split()]):
+				if 'calyx' in feat.lower() and key != 'CalyxSize' or 'petiole' in feat.lower() and key != 'PetioleSize' or 'anther' in feat.lower() and key != 'AntherSize' or 'pedicel' in feat.lower() and key != 'PedicelSize':
+					continue
+				word_match = [w.lower() for w in feat.split() if w.lower() in set(values)][0]
+				
+				if found=='InflorescenceSize' and key == 'FlowerSize': # if inflorencence was already found, skip flower (e.g., "flower stem" associated with inflorescence only)
+					continue
+				if found=='FlowerSize' and key in ['StamenSize']: # avoid matching cases such as "Flowers large, white, about 8mm across, 4-petalled with 6 __stamens__"
+					continue
+				if found == 'FruitSize' and not any([w.lower() in feat.lower() for w in ['achene', 'cypsela']]):
+					continue
+				# measure is a number and a unit of measurement (e.g. 1.5 mm)
+				# can match forms like 1.5-2.5 mm, 1.5 – 2.5 mm, 1.5×2.5 mm
+				measure = re.search(full_regex, feat)
+				if measure:
+					word_match_position = np.where(np.array(feat.lower().split()) == word_match)[0][0]
+					measure_position = np.where(np.array(feat.lower().split()) == measure.group())[0][0]
+
+					if abs(word_match_position - measure_position) > wordmeasure_distance:
+						continue
+					if found:
+						if (any([w.lower() in feat.lower() for w in ['achene', 'cypsela']]) and {key, found[0]} == {'FruitSize', 'SeedSize'}) or\
+							{key, found[0]} == {'StigmaSize', 'StyleSize'}:
+							pass
+							# print(f'OK>> Multiple features found ({found}, {key}) in "{feat}"')
+						else:
+							this_distance = abs(word_match_position - measure_position)
+							if this_distance > found[1]:
+								continue
+							else:
+								features[found[0]].remove(found[2])
+							tmp.append(f'({i}) Multiple features found ({found[0]}, {key}) in "{feat}"')
+							anomalies.add(i)
+					found = (key, abs(word_match_position - measure_position), measure.group())
+					measure = measure.group()
+					# print(key,[w.lower() for w in set(values)|set([key]) if w.lower() in feat.lower()], measure)
+					
+					if key in features:
+						features[key].append(measure)
+					else:
+						features[key] = [measure]
+			# primary features, if key is in petiole, anther, pedicel, calix
+	return pd.Series(features)
+
+
+def get_unlabelled_measures(processed_features):
+	for i, (species_name, feature) in enumerate(processed_features.iterrows()):
+		features_text = species.loc[species_name, 'Features'].replace('\xa0', ' ').replace('×', 'x').replace('–', '-')
+		if features_text == '': continue
+		
+		measures_in_text = re.finditer(r'\d[\d\.\s\(\)x-]*[cmd]?m\s?(?![\d-\(\)])', features_text)
+		for match in measures_in_text:
+			which_feature = feature[feature.apply(lambda x: isinstance(x, str) and string_preprocessing(match.group()).strip() in x.split('; '))]
+			if which_feature.empty:
+				anomalies.add(species_name)
