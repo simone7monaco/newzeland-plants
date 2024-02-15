18d5bdce7acda6233a241de8533e0492440d3851
simone7monaco <simone7monaco@gmail.com>
2024-02-15 19:05:41 +0100

    final fix at controller

--- Felci/utils.py
+++ Felci/utils.py
@@ -23,13 +23,14 @@ full_regex = rf"(({number}\s?-\s?)?{number})?({number}\s?-\s?)?{number}\s*{unit}
 full_regex = full_regex + r'(?=[\s\.,;])'
 
 
+
 def string_preprocessing(s):
 	s = s.replace('\xa0', ' ').replace('×', 'x').replace('–', '-').replace('·', '.') # remove non-breaking space and replace multiplication sign with x
 	s = re.sub(r'(?<=xcluding)\s+[\w-]+', ' ', s) # remove each word following "excluding" (Mericarps (excluding style) 2.5-3.0 mm should point to "Mericarps")
 	s = re.sub(fr'-?\(-?{number}-?\)-?', '', s) # remove all parentesis surrounding a number and the number inside (e.g. (-1.5) --> **)
-	s = re.sub(r'(?<=\d)\s+(?=[cmd]?m)', '', s) # remove all spaces before measures (mm, cm, dm, m, these strings only if padded by a space)
-	# s = re.sub('m long;?', 'm-long', s) # remove space between measure and "long" (e.g. 2 mm long --> 2 mm-long)
-	# s = re.sub('m wide;?', 'm-wide', s) # remove space between measure and "wide" (e.g. 2 mm wide --> 2 mm-wide)
+	s = re.sub(rf'(?<=\d)\s+(?={unit})', '', s) # remove all spaces before measures (mm, cm, dm, m, these strings only if padded by a space)
+	s = re.sub('m long;?', 'm-long', s) # remove space between measure and "long" (e.g. 2 mm long --> 2 mm-long)
+	s = re.sub('m wide;?', 'm-wide', s) # remove space between measure and "wide" (e.g. 2 mm wide --> 2 mm-wide)
 	s = re.sub(r'\s*-\s*', '-', s) # remove spaces around hyphens
 	s = re.sub(r'(?<=\d)\s*\.(?=\d)', '.', s) # remove spaces before dot if followed and preceded by a number
 	s = re.sub(r'(?<=\s)\.(?=\d)', '0.', s) # add a 0 before a dot if it is preceded by a space and a "not number" and followed by a number (e.g. foo .5 --> foo 0.5)
@@ -39,6 +40,7 @@ def string_preprocessing(s):
 
 tmp = []
 anomalies = set() # species with anomalies
+
 
 def extract_features(i, feats:list):
 	features = {}
@@ -50,38 +52,45 @@ def extract_features(i, feats:list):
 		for measure in measures:
 			found = None
 			for key, values in extra_features.items():
+				if key.startswith('Habit'):
+					# categorical feature, append all the values present in feat
+					features['Habit'] = ';'.join([v for v in values if v.lower() in re.split(r'[^\w]', feat.lower())])
+					continue
+				if key.startswith('Venation'):
+					# categorical feature, store feat as it is
+					# features['Venation'] = feat
+					continue
 
 				feat = feat[:-1] if feat[-1] in ['.', ';'] else feat # remove any of .; at the end of the sentence
 				matched_word = list(re.finditer( r'\b('+ '|'.join([w for w in set(values)]) + r')\b', feat.lower()))
 				
-				if any(matched_word):	
-					if key != 'Stature': # TODO: Caso up to 3m, petiole 2mm???
-						matched_word = [w for w in matched_word if w.span()[0] < measure.span()[0]]
-						if not any(matched_word):
-							continue # if not stature and measure appears before the word, skip
+				if any(matched_word):
+				# "*Secondary* pinnae decreasing very gradually in length along each ~primary~ pinna to the distal end..." is supposed to be Secondary
+					if key == 'PrimaryPinnae' and 'secondary' in feat.lower() and features.get('PrimaryPinnae') is not None:
+						continue
+					matched_word = [w for w in matched_word if w.span()[0] < measure.span()[0]]
+					if not any(matched_word):
+						continue
 					matched_word = sorted(matched_word, key=lambda word: word.span()[1] - measure.span()[0])[0]
-
 					this_distance = abs(matched_word.span()[1] - measure.span()[0])
 					# this_distance = abs(word_match_position - measure_position)
+
+					if key == 'Stipe':
+						hair_or_scale_position = list(re.finditer(r'(hair|scale)', feat.lower()))
+						# C1: Se nella frase dello stipe trovi le keyword "hair", "hairs", "scale", "scales", i valori dopo queste keyword vanno ignorati.
+						if any(hair_or_scale_position) and hair_or_scale_position[0].start() < measure.start():
+							continue
 					if found:
-						if (any([w in feat.lower() for w in ['achene', 'cypsela']]) and {key, found[0]} == {'FruitSize', 'SeedSize'}) or\
-						   (any([w in feat.lower() for w in ['stigma-style']]) and {key, found[0]} == {'StigmaSize', 'StyleSize'}) or\
-						   (any([w in feat.lower() for w in ['floret']]) and {key, found[0]} == {'RayFloretsSize', 'DiskFloretSize'}):
-							pass
-							# print(f'OK>> Multiple features found ({found}, {key}) in "{feat}"')
-						else:
-							if this_distance >= found[1]:
-								continue
-							else:
-								# print(found, key, matched_word, measure)
-								features[found[0]].remove(found[2])
-							anomalies.add(i)
-					found = (key, this_distance, measure.group().strip())
-					
+						if this_distance >= found[1]:
+							continue
+						features[found[0]].remove(found[2])
+						anomalies.add(i)
+
+					found = (key, this_distance, measure.group())
 					if key in features:
-						features[key].append(measure.group().strip())
+						features[key].append(measure.group())
 					else:
-						features[key] = [measure.group().strip()]
+						features[key] = [measure.group()]
 	return pd.Series(features)
 
 def get_unlabelled_measures(processed_features):
